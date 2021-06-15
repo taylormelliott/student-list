8934e672e7352506bf0829e2caf4149a711e103e
Taylor Elliott <tmikale@gmail.com>
2021-06-15 09:37:17 -0600

    Added RollBar

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const path = require("path");
+const Rollbar = require("rollbar");
+const rollbar = new Rollbar({
+  accessToken: "34fe08ef13184a76b5699e7cee582ad7",
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
+  rollbar.info("html file served successfully");
 });
 
 const port = process.env.PORT || 4444;
